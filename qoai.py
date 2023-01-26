18169ae5f9dc9377cac0b9b45b3d390f6cc72ece
Castedo Ellerman <castedo@castedo.com>
2023-01-26 02:19:16 +0000

    diff adapt OpenAI GPT revision text to orig text

--- qoai.py
+++ qoai.py
@@ -9,20 +9,11 @@ XDG_BASE_DIRS = dict(
     XDG_STATE_HOME="~/.local/state",
 )
 
-try:
-    import nltk  # type: ignore
-except ImportError:
-    pass
-
-try:
-    import jsoml  # type: ignore
-except ImportError:
-    pass
-
 # Python Standard Library
-import argparse, json, os, sys
+import argparse, json, os, re, sys
 from datetime import datetime
 from pathlib import Path
+from difflib import SequenceMatcher
 
 
 def get_xdg_path(xdg_dir_var: str, subpath) -> Path:
@@ -42,43 +33,83 @@ def live_query_openai(req):
     return openai.Completion.create(**req)
 
 
-def query_openai_by_file(settings_path, source_path, log_path):
-
-    with open(source_path) as file:
-        source = file.read()
-
+def make_openai_request(settings_path, source):
     if settings_path.suffix == ".json":
         with open(settings_path) as file:
             settings = json.load(file)
     else:
-        import jsoml
+        import jsoml  # type: ignore
+
         settings = jsoml.load(settings_path)
 
-    oai_req = settings["openai"]
-    oai_req["max_tokens"] = int(settings["max_tokens_ratio"] * len(source) / 4)
-    oai_req["prompt"] = settings["prepend"] + source + settings["append"]
-
-    print("OpenAI query for", source_path)
-    response = live_query_openai(oai_req)
-    log_openai_query(source_path, oai_req, response, log_path)
-    return response
+    ret = settings["openai"]
+    ret["max_tokens"] = int(settings["max_tokens_ratio"] * len(source) / 4)
+    ret["prompt"] = settings["prepend"] + source + settings["append"]
+    return ret
 
 
-def log_openai_query(source_path, request, response, log_path) -> None:
-    t = datetime.utcfromtimestamp(response['created'])
+def log_openai_query(name, request, response, log_path) -> None:
+    t = datetime.utcfromtimestamp(response["created"])
     ts = t.isoformat().replace("-", "").replace(":", "") + "Z"
     data = dict(request=request, response=response)
     if log_path is None:
         log_path = get_xdg_path(*QOAI_LOG_DIR)
         os.makedirs(log_path, exist_ok=True)
-    save_stem = source_path.stem + "." + ts
+    save_stem = name + "." + ts
     print("Saving OpenAI response", save_stem)
-    if 'jsoml' in sys.modules:
+    try:
+        import jsoml  # type: ignore
+
         jsoml.dump(data, log_path / (save_stem + ".xml"))
-    else:
-        with open(log_path / (save_stem + ".json"), 'w') as file:
+    except ImportError:
+        with open(log_path / (save_stem + ".json"), "w") as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
             file.write("\n")
+
+
+def tokenize(pattern, text):
+    return [match[0] for match in pattern.finditer(text)]
+
+
+def diffadapt(orig_text, revisions):
+    ret = []
+    re_token = re.compile(r"\w+|\W|\n")
+    isjunk = lambda x: x == " "
+    matcher = SequenceMatcher(isjunk)
+    # ... caches detailed information about the second sequence,
+    # so if you want to compare one sequence against many sequences,
+    # use set_seq2() ...
+    orig = tokenize(re_token, orig_text)
+    matcher.set_seq2(orig)
+    for rev_text in revisions:
+        adapted = []
+        rev = tokenize(re_token, rev_text)
+        matcher.set_seq1(rev)
+        # get_opcodes for converting revised text back to orig
+        for tag , r1, r2, o1, o2 in matcher.get_opcodes():
+            # tag meaning is relative to going from revised text back to orig
+            if tag in ['replace', 'insert'] and "\n" in orig[o1:o2]:
+                adapted += ["\n"]
+            adapted += rev[r1:r2]
+        ret.append("".join(adapted))
+    return ret
+
+
+def write_revisions(outpath_pattern, source, revisions):
+    texts = []
+    try:
+        import nltk  # type: ignore
+
+        for rev in revisions:
+            lines = nltk.sent_tokenize(rev.lstrip())
+            texts.append("\n".join(lines + [""]))
+    except ImportError:
+        texts = revisions
+
+    revisions = diffadapt(source, texts)
+    for i, out_text in enumerate(revisions):
+        with open(outpath_pattern.format(i + 1), "w") as file:
+            file.write(out_text)
 
 
 def main(cmd_line_args=None):
@@ -96,16 +127,15 @@ def main(cmd_line_args=None):
     else:
         if args.set is None:
             args.set = get_xdg_path(*QOAI_DEFAULT_SET_FILE)
-        response = query_openai_by_file(args.set, args.source, args.log)
+        print("OpenAI query for", args.source)
+        with open(args.source) as file:
+            source = file.read()
+        request = make_openai_request(args.set, source)
+        response = live_query_openai(request)
+        log_openai_query(args.source.stem, request, response, args.log)
         print("Writing", outpath.format("*"))
-        for i, choice in enumerate(response["choices"]):
-            out_text = choice["text"]
-            with open(outpath.format(i + 1), "w") as file:
-                if "nltk" in sys.modules:
-                    for line in nltk.sent_tokenize(out_text.lstrip()):
-                        print(line, file=file)
-                else:
-                    file.write(out_text)
+        revisions = [c["text"] for c in response["choices"]]
+        write_revisions(outpath, source, revisions)
 
 
 if __name__ == "__main__":
--- tests/test_qoai.py
+++ tests/test_qoai.py
@@ -23,3 +23,35 @@ def test_main(tmp_path):
     qoai.main(["--set", "set/grammar.xml", srcpath, "--log", str(tmp_path)])
     got = open(srcpath + ".R1").read()
     assert got == TEXT_LINED
+
+
+def test_simple_diffs():
+    assert qoai.diffadapt("Whatever", [""]) == [""]
+    assert qoai.diffadapt("Hello", ["World"]) == ["World"]
+
+
+def test_jupiter_diff():
+
+    orig = """Jupiter is the largest planet
+ in the Solar System.
+Jupiter is the fifth planet
+ from the Sun.
+Jupiter is classified as a gas giant.
+This is because
+ Jupiter is very big
+ and made up of gas.
+"""
+
+    revised = """Jupiter is the largest planet in the Solar System and the fifth planet from the Sun.
+It is classified as a gas giant due to its immense size and composition of gas."""
+
+    expected = """Jupiter is the largest planet
+ in the Solar System
+ and the fifth planet
+ from the Sun.
+It is classified as a gas giant
+ due to its immense size and composition of gas.
+"""
+
+    got = qoai.diffadapt(orig, [revised])[0]
+    assert got == expected
